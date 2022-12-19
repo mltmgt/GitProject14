b8fbcb1bf4df904aa4a2b37bf44e05a8499b7494
mltmgt <meltem.zumba@gmail.com>
2022-12-20 01:13:51 +0300

    New codes added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("version is complete");
+    public static void main(String[] args) {
+        SolarSystem mel=new SolarSystem();
+        mel.sun=1;
+        mel.moon=2;
+        mel.planet=3;
+
     }
 }
